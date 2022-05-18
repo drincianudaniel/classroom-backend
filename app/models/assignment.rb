b0262a72fa46060602c10df34456ce3640e8c7c8
DESKTOP-NGEPSM5\BEST <larisaoprea333@yahoo.com>
2022-05-18 11:14:09 -0700

    dependency destroy

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,8 +1,8 @@
 class Assignment < ActiveRecord::Base
-    has_many :userassignments
+    has_many :userassignments, dependent: :destroy
     has_many :users, through: :userassignments
 
     belongs_to :classroom
 
-    has_many :solutions
+    has_many :solutions, dependent: :destroy
 end
--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,7 +1,7 @@
 class Classroom < ActiveRecord::Base
     # has_and_belongs_to_many :users
-    has_many :userclassrooms
+    has_many :userclassrooms, dependent: :destroy
     has_many :users, through: :userclassrooms
     
-    has_many :assignments
+    has_many :assignments, dependent: :destroy
 end
