fbf16e401d722d9abefad5b5d3eac595a222c7ec
Dani <drincianu.dani@gmail.com>
2022-05-24 11:30:49 +0300

    more login tests

--- spec/login_controller_spec.rb
+++ spec/login_controller_spec.rb
@@ -10,18 +10,31 @@ RSpec.describe SessionsController, type: :controller do
             post :create, params:{user:{email:'admin@gmail.com', password: '123'}}
             data = JSON.parse(response.body)
             expect(response.status).to eq(200)
+            post :is_logged_in?
+            data = JSON.parse(response.body)
+            expect(data["logged_in"]).to eq(true)
         end
 
         it 'Login with incorrect credentials' do
             post :create, params:{user:{email:'admin123@gmail.com', password: '1234'}}
             data = JSON.parse(response.body)
             expect(response.status).to eq(401)
+            post :is_logged_in?
+            data = JSON.parse(response.body)
+            expect(data["logged_in"]).to eq(false)
         end
 
         it 'Logout test' do
+            session[:user_id] = @user.id
+            post :is_logged_in?
+            data = JSON.parse(response.body)
+            expect(data["logged_in"]).to eq(true)
             delete :destroy
             data = JSON.parse(response.body)
             expect(response.status).to eq(200)
+            post :is_logged_in?
+            data = JSON.parse(response.body)
+            expect(data["logged_in"]).to eq(false)
         end
     end
 end
